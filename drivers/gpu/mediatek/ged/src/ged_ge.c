a4593f7a89c4793befb1096de9fbabfb7c90fb32
victor-cl.wu <victor-cl.wu@mediatek.com>
2022-08-20 14:21:33 +0800

    [ALPS07536572] GED: Security Vulnerability integer overflow
    1. integer overflow
    2. buffer size checking
    
    MTK-Commit-Id: 31f6d9b80668d458e9f8a7f21db4f1f858f52239
    
    CR-Id: ALPS07536572
    Feature: GED driver
    Change-Id: Ic859445cb11d2fa31a259c3f32f62f5fd068d1c0
    Signed-off-by: victor-cl.wu <victor-cl.wu@mediatek.com>
    (cherry-pick from c1cfffa00342b3089dfb64b2bf7a26efb3eb6886)

--- drivers/gpu/mediatek/ged/src/ged_ge.c
+++ drivers/gpu/mediatek/ged/src/ged_ge.c
@@ -154,7 +154,9 @@ int ged_ge_alloc(int region_num, uint32_t *region_sizes)
 		// check region_sizes parameter
 		if (region_sizes[i] <= 0 ||
 			region_sizes[i] > GE_MAX_REGION_SIZE) {
-			goto err_kmalloc;
+			GED_PDEBUG("check size fail rgion_sizes[%d]:%u\n",
+				i, region_sizes[i]);
+				goto err_kmalloc;
 		}
 		entry->region_sizes[i] = region_sizes[i];
 	}
