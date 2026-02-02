d310cba7571c76c7e763e053a3ca5d24c78d51bc
HELLINFIX <mridulsharma57570@gmail.com>
2026-02-02 19:50:51 +0000

    kernel: don't allow userspace to override swappiness

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -109,6 +109,10 @@ extern unsigned int sysctl_sched_walt_init_task_load_pct;
 
 #if defined(CONFIG_SYSCTL)
 
+static int android_swappiness_min = 100;
+static int android_swappiness_max = 160;
+
+
 /* External variables not in a header file. */
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
@@ -1602,8 +1606,9 @@ static struct ctl_table vm_table[] = {
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
-		.extra2		= &one_hundred,
+		.extra1 = &android_swappiness_min,
+		.extra2 = &android_swappiness_max,
+
 	},
 #ifdef CONFIG_DYNAMIC_TUNNING_SWAPPINESS
 	{
