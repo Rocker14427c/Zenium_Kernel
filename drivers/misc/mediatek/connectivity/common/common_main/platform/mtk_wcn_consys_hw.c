db963bb8d937d01e3bf359eae7e6a60bcb8a08fa
Badmaneers <dukebraham24@gmail.com>
2026-05-23 13:45:39 +0530

    fix: remove unused regmap struct definition for compatibility with kernel version

--- drivers/misc/mediatek/connectivity/common/common_main/platform/mtk_wcn_consys_hw.c
+++ drivers/misc/mediatek/connectivity/common/common_main/platform/mtk_wcn_consys_hw.c
@@ -170,10 +170,6 @@ struct pinctrl *consys_pinctrl;
 struct work_struct plt_resume_worker;
 static void plat_resume_handler(struct work_struct *work);
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0))
-struct regmap *g_regmap;
-#endif
-
 static int wmt_thermal_get_temp_cb(void *data, int *temp);
 static const struct thermal_zone_of_device_ops tz_wmt_thermal_ops = {
 	.get_temp = wmt_thermal_get_temp_cb,
